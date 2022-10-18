029b9458c8005216ae1792f762cd7996d7f10ae0
Sehaj bindra <sehajbindra1234@gmail.com>
2022-10-18 17:20:07 +0530

    styling changes

--- .history/tailwind.config_20221018171955.js
+++ .history/tailwind.config_20221018171955.js
@@ -2,10 +2,13 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./Components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/line-clamp"),
+    require("tailwind-scrollbar-hide"),
+  ],
 };
